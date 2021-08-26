7a9d2bf801461b50b2fb7b7d1ff9ca5ece66fe8b
Alexander Peters <businessonly1011@gmail.com>
2021-08-26 19:31:10 -0400

    Nightly push

--- convert.py
+++ convert.py
@@ -1,1 +1,10 @@
-print('hello world')
+import argparse
+import csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename')
+args = parser.parse_args()
+
+with open(args.filename, neline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+        
