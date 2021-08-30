e13fb4f5ecb36a905f9e1f82741ed81b1eab2c42
Alexander Peters <businessonly1011@gmail.com>
2021-08-30 13:08:21 -0400

    File reading working

--- convert.py
+++ convert.py
@@ -1,10 +1,28 @@
 import argparse
-import csv
+import numpy as np
+import re
 
+# Parse user args
 parser = argparse.ArgumentParser()
-parser.add_argument('filename')
+parser.add_argument('fileIn')
+parser.add_argument('fileOut')
 args = parser.parse_args()
 
-with open(args.filename, neline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-        
+# Remove special chars that may confuse Numpy
+with open(args.fileIn, 'r') as file :
+  filedata = file.read()
+
+special_char = '"@_!#$%^&*()<>?/\|}{~:;[]'
+for i in special_char:
+    filedata = filedata.replace(i, '')
+
+with open(args.fileIn, 'w') as file:
+  file.write(filedata)
+
+# Numpy read in csv
+dataIn = np.genfromtxt(args.fileIn, delimiter=',', dtype='U70', skip_header=True)
+    
+partNames = dataIn[:,0]
+xDist = dataIn[:,3]
+yDist = dataIn[:,4]
+rot = dataIn[:,5]
